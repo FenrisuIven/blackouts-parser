89f91a080981f77f1151f5a77882f3b981c05cf4
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2024-07-17 21:30:53 +0300

    change table to vertical layout

--- src/table-constructor.js
+++ src/table-constructor.js
@@ -2,8 +2,9 @@ import {data} from "./parser.js"
 
 export function firstInit(table) {
     insertHourCells(table)
+    const tr = table.rows[0]
+    tr.insertCell()
     for (let i = 1; i < 7; i++) {
-        const tr = table.insertRow()
         const cell = tr.insertCell()
         const span = document.createElement('span')
         span.textContent = `${i} черга`
@@ -15,32 +16,43 @@ export function firstInit(table) {
 export function fillTable() {
     const table = document.querySelector('#blackouts-order-table')
 
-    for (let i = 1; i < 7; i++) {
+    console.log(data)
+    for (let i = 1; i <= 24; i++) {
         const tr = table.rows[i]
-        insertDataInCells(tr, data, i)
+        insertDataInCells(table, data, i)
     }
 }
 
-function insertDataInCells(tr, data, queueNum) {
-    data.forEach(currCol => {
-        let td = tr.children[data.indexOf(currCol) + 1]
-        if (!td) td = tr.insertCell()
-        const isQueueTurnedOff = currCol.includes(queueNum.toString())
-        td.style.background = isQueueTurnedOff ? 'salmon' : 'lightgray'
-    })
+function insertDataInCells(table, data, hourNum) {
+    let idx = 0
+    for (let i = 0; i < 6; i++) {
+        let cell = table.rows[hourNum][i]
+        if (!cell) cell = table.rows[hourNum].insertCell()
+        const isQueueTurnedOff = data[hourNum - 1][idx] === (i + 1).toString()
+        
+        cell.style.background = isQueueTurnedOff ? 'sandybrown' : ''
+        cell.style.border = '1px solid dimgray'
+        
+        if (isQueueTurnedOff) {
+            idx++
+        }
+    }
 }
 
 function insertHourCells(table) {
-    const row = table.insertRow()
-    row.insertCell()
+    table.insertRow()
     for(let i = 1; i <= 24; i++){
+        const row = table.insertRow()
         const cell = row.insertCell()
         
         let text = `${i-1}:00 - `.padStart(8,'0')
         text += `${i}:00`.padStart(5,'0')
         const span = document.createElement('span')
         span.textContent = text
-        
+        span.style.width = 'max-content'
+
         cell.appendChild(span)
+        cell.style.textAlign = 'center'
+        cell.classList.add('hour-cell')
     }
 }
